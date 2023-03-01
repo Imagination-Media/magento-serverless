cb8ff8cd7151509ced3985c62558017681d2c8a7
Igor Ludgero Miura <igor@imaginationmedia.com>
2023-03-01 15:44:45 -0500

    Add header to all files

--- Model/ResourceModel/ServerlessFunction/Collection.php
+++ Model/ResourceModel/ServerlessFunction/Collection.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Serverless
+ *
+ * Use serverless function to modify the Magento business logic.
+ *
+ * @package ImDigital\Serverless
+ * @author Igor Ludgero Miura <igor@imdigital.com>
+ * @copyright Copyright (c) 2023 Imagination Media (https://www.imdigital.com/)
+ * @license Private
+ */
+
 declare(strict_types=1);
 
 namespace ImDigital\Serverless\Model\ResourceModel\ServerlessFunction;
